0d6c9978b7a9d5d75e1677aae06dcc65f94e94e0
Balaje K <balaje6@gmail.com>
2022-05-20 14:41:57 +0930

    Begin module to implement complex resonances

---
+++ src/ComplexResonances/ComplexResonances.jl
@@ -0,0 +1,12 @@
+module ComplexResonances
+
+using PolynomialRoots
+using LinearAlgebra
+
+import iceFEM.NonDimProblem: Fluid, Ice
+import iceFEM.NonDimProblem: NonDimensionalProblem, non_dimensionalize
+import iceFEM.NonDimProblem: FreeBedrock, FreeClamped, FreeHinged, FreeFree
+
+import iceFEM.Models: FiniteDepth, solve, FiniteDepthSolution
+
+end
---
+++ src/ComplexResonances/InterpolateFreq.jl
@@ -0,0 +1,9 @@
+struct CoarseSolution <: Any
+
+end
+struct FineSolution <: Any
+
+end
+function
+
+end
