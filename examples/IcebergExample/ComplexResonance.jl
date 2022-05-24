d0629a38c09ab5c2a1195bdae3ce548f6b4e9218
Balaje K <balaje6@gmail.com>
2022-05-24 21:18:24 +0930

    Rename and Cleanup

--- examples/IcebergExample/ComplexResonance.jl
+++ examples/IcebergExample/ComplexResonance.jl
@@ -23,29 +23,29 @@ fluid = Fluid(ρₒ, 0, g, H, 0)
 BeamType=FreeFree()
 NModes=3
 WaterType=FiniteDepth(NModes)
-# ωᵣ = 2π*LinRange(0.01,0.125,200)
-# Ω₁ = FreqSpace{Float64}(4*NModes+6, length(ωᵣ)) # Real Coarse-Frequency space
-# for i in 1:length(ωᵣ)
-#   fd = solve(ice, fluid, ωᵣ[i], BeamType, WaterType)
-#   Ω₁.ω[i] = ωᵣ[i]
-#   Ω₁.H₀[i] = fd.K
-#   Ω₁.f₀[i] = reshape(fd.f,length(fd.f),1)
-# end
-# ω₁s,H₁s,f₁s = InterpolateFreqDomain(Ω₁, (1600,));
-# for i in 1:length(f₁s)
-#   Aₚ = 9.8/(1im*ω₁s[i])
-#   λ = (H₁s[i])\(f₁s[i])
-#   a₀ = λ[1]/Aₚ
-#   d₀ = λ[3NModes+8]/Aₚ
-# end
+ωᵣ = 2π*LinRange(0.01,0.125,200)
+Ω₁ = FreqSpace{Float64}(4*NModes+6, length(ωᵣ)) # Real Coarse-Frequency space
+for i in 1:length(ωᵣ)
+  fd = solve(ice, fluid, ωᵣ[i], BeamType, WaterType)
+  Ω₁.ω[i] = ωᵣ[i]
+  Ω₁.H₀[i] = fd.K
+  Ω₁.f₀[i] = reshape(fd.f,length(fd.f),1)
+end
+ω₁s,H₁s,f₁s = InterpolateFreqDomain(Ω₁, (1600,));
+for i in 1:length(f₁s)
+  Aₚ = 9.8/(1im*ω₁s[i])
+  λ = (H₁s[i])\(f₁s[i])
+  a₀ = λ[1]/Aₚ
+  d₀ = λ[3NModes+8]/Aₚ
+end
 
-# #############
-# # Visualize #
-# #############
-# plt = plot(ωᵣ, [abs.((Ω₁.H₀[i]\Ω₁.f₀[i]))[1]*ωᵣ[i]/9.8 for i in 1:length(ωᵣ)],
-#            linecolor=:red, linestyle=:dash, label="Coarse Solution")
-# plot!(plt, ω₁s, [abs.((H₁s[i]\f₁s[i]))[1]*ω₁s[i]/9.8 for i in 1:length(ω₁s)],
-#      linecolor=:blue, linestyle=:solid, label="Refined Solution")
+#############
+# Visualize #
+#############
+plt = plot(ωᵣ, [abs.((Ω₁.H₀[i]\Ω₁.f₀[i]))[1]*ωᵣ[i]/9.8 for i in 1:length(ωᵣ)],
+           linecolor=:red, linestyle=:dash, label="Coarse Solution")
+plot!(plt, ω₁s, [abs.((H₁s[i]\f₁s[i]))[1]*ω₁s[i]/9.8 for i in 1:length(ω₁s)],
+     linecolor=:blue, linestyle=:solid, label="Refined Solution")
 
 ###########################################################################
 # Solve a set of frequency domain problems in the complex frequency space #
@@ -53,28 +53,28 @@ WaterType=FiniteDepth(NModes)
 xc = 2π*LinRange(0.01,0.04,50)
 yc = LinRange(-0.06,0.06,50)
 ωc = (xc' .* ones(length(yc))) + 1im*(ones(length(xc))' .* yc)
-# Ω₂ = FreqSpace{ComplexF64}(4*NModes+6, size(ωc,1))
-# for i in 1:size(ωc,1)
-#   for j in 1:size(ωc,2)
-#     fd = solve(ice, fluid, ωc[i,j], BeamType, WaterType)
-#     Ω₂.ω[i,j] = ωc[i,j]
-#     Ω₂.H₀[i,j] = fd.K
-#     Ω₂.f₀[i,j] = reshape(fd.f,length(fd.f),1)
-#   end
-# end
-# ω₂s,H₂s,f₂s = InterpolateFreqDomain(Ω₂, (300,300));
-#############
-# Visualize #
-#############
-# a₀s = zeros(ComplexF64,size(ω₂s))
-# d₀s = zeros(ComplexF64,size(ω₂s))
-# for i in 1:size(a₀s,1)
-#   for j in 1:size(a₀s,2)
-#     Aₚ = 9.8/(1im*ω₂s[i,j])
-#     a₀s[i,j] = ((H₂s[i,j])\(f₂s[i,j]))[1]/Aₚ
-#     d₀s[i,j] = ((H₂s[i,j])\(f₂s[i,j]))[3NModes+8]/Aₚ
-#   end
-# end
+Ω₂ = FreqSpace{ComplexF64}(4*NModes+6, size(ωc,1))
+for i in 1:size(ωc,1)
+  for j in 1:size(ωc,2)
+    fd = solve(ice, fluid, ωc[i,j], BeamType, WaterType)
+    Ω₂.ω[i,j] = ωc[i,j]
+    Ω₂.H₀[i,j] = fd.K
+    Ω₂.f₀[i,j] = reshape(fd.f,length(fd.f),1)
+  end
+end
+ω₂s,H₂s,f₂s = InterpolateFreqDomain(Ω₂, (300,300));
+
+Visualize #
+
+a₀s = zeros(ComplexF64,size(ω₂s))
+d₀s = zeros(ComplexF64,size(ω₂s))
+for i in 1:size(a₀s,1)
+  for j in 1:size(a₀s,2)
+    Aₚ = 9.8/(1im*ω₂s[i,j])
+    a₀s[i,j] = ((H₂s[i,j])\(f₂s[i,j]))[1]/Aₚ
+    d₀s[i,j] = ((H₂s[i,j])\(f₂s[i,j]))[3NModes+8]/Aₚ
+  end
+end
 Rω = portrait(a₀s, PTproper, ctype="nist");
 Tω = portrait(d₀s, PTproper, ctype="nist");
 plt1 = plot(LinRange(0.01,0.04,300), LinRange(-0.06,0.06,300)/(2π), reverse(Rω,dims=1))
--- examples/IcebergExample/ResonanceVsYoungs.jl
+++ examples/IcebergExample/ResonanceVsYoungs.jl
@@ -16,35 +16,6 @@ h = 280
 g = 9.8
 H = 500
 
-function computeResonanceFrequency(ice, fluid, ω₀, verbosity=0)
-  count = 1
-  dw = 1e-6
-  tol=1e-40
-  fd = FiniteDepth(4)
-  Δw = 1
-  while (abs(Δw) > tol) && (count ≤ 50)
-    s₁ = solve(ice, fluid, ω₀, FreeFree(), fd)
-    s₂ = solve(ice, fluid, ω₀ + Δw, FreeFree(), fd)
-    Hⁿ = s₁.K
-    Hⁿ⁺¹ = s₂.K
-
-    ΔH = (Hⁿ⁺¹ - Hⁿ)/Δw
-    dwₛ = eigvals(-ΔH\Hⁿ)
-    (verbosity > 0) && print(string(Δw)*"\n")
-    #dwₛ = eigvals(Hⁿ, -ΔH)
-    dwₛ = sort(dwₛ,by=x->abs(x))
-    Δw = dwₛ[1]
-    #@time condH = cond(Hⁿ)
-    (verbosity > 1) && print("Δw = "*string(Δw)*"\t ω₀ = "*string(ω₀)*"\t cond(H) = "*string(condH)*"...\n")
-    if(!isinf(abs(Δw)))
-      ω₀ = ω₀ + Δw
-    end
-    count+=1
-  end
-  (verbosity == 1) && print("Number of iterations = "*string(count)*"\n")
-  ω₀
-end
-
 Es = LinRange(1,5,30)
 ω₀s = 2π*[0.022, 0.051]
 ωₑs = [1/40, 1/15]
